a47a6df173599ef37fd8b829c5a37b48f111bd79
system-error <christos_dousmanopoulos@yahoo.gr>
2020-02-05 13:04:06 +0200

    new changes

--- reservation.py
+++ reservation.py
@@ -1,10 +1,12 @@
+from room import Room
+
 class Reservation:
     days = [1,2,3,4,5,6,7,8,9,10,
             11,12,13,14,15,16,17,18,19,20,
             21,22,23,24,25,26,27,28,29,30,31]
     months = [1,2,3,4,5,6,7,8,9,10,11,12]
     year = 2020
-    numberOfDays=0
+    numberOfDays=1
 
     # def __init__(self):
     #     self.days
@@ -14,15 +16,19 @@ class Reservation:
     
 
     def makeReservation(self):
-        # l.__contains__(3)
+        
         startDate = []
         endDate = []
         counter = 0
-        print(len(startDate))
+        start = "start"
+        end = "end"
         
         while counter<2:
 
-            day = int(input("Give me the day in number from 1-31 "))
+            if (counter<1):
+                day = int(input(f"Give me the {start} day in number from 1-31 "))
+            else:
+                day = int(input(f"Give me the {end} day in number from 1-31 "))
             if self.days.__contains__(day):
                 if len(startDate) < 3:
                     startDate.append(day)
@@ -32,7 +38,10 @@ class Reservation:
                 print("Wrong number, try again")
                 counter = 2
 
-            month = int(input("Give me the month in number from 1-12 "))
+            if (counter<1):
+                month = int(input(f"Give me the {start} month in number from 1-12 "))
+            else:
+                month = int(input(f"Give me the {end} month in number from 1-12 "))
             if self.months.__contains__(month):
                 if len(startDate) < 3:
                     startDate.append(month)
@@ -48,8 +57,49 @@ class Reservation:
                 endDate.append(self.year)
             counter +=1    
         return startDate,endDate
-            
-                
+        
+
+
+    def availability(self,startDates,endDates,room):
+        # print(Room.roomsAndBeds[room])
+        # test=len(Room.roomsAndBeds)
+        for x in range(0, len(Room.roomsAndBeds)):
+            for y in range (len(Room.startDateReserved[x])):
+                if(((Room.startDateReserved[x][y][0] < startDates[0]) and (Room.startDateReserved[x][y][1] == startDates[1])) 
+                    or ((Room.endDateReserved[x][y][0] < endDates[0]) and (Room.endDateReserved[x][y][1] == endDates[1]))) :
+                    print("test")
+
+
+    def displayTheRooms(self):
+        print("Our Rooms")
+        print("")
+        for room in Room.roomsAndBeds:
+            print(f"====== {room}  ======")
+            print(f"The number of beds for this room are: {Room.roomsAndBeds[room]['beds']}")
+            print("=================")
+            print(f"The price for this room is: {Room.roomsAndBeds[room]['price']} €")
+            print("")
+
+    def greetings(self):
+        pass                           
+
+# re = Reservation()
+
+# startDate = re.makeReservation()
+
+r = Room()
+
+# re = Reservation()
+
+# startDate,endDate = re.makeReservation()
+
+# re.availability(startDate,endDate,r.roomsAndBeds['room1'])
+
+
+# ro = Room()
+
+
+
 
 
 
--- room.py
+++ room.py
@@ -1,40 +1,48 @@
-from reservation import Reservation
+# from reservation import Reservation
 
 class Room:
+    
+    startDateReserved = [] 
+    endDateReserved = []
     roomsAndBeds = {
         "room1":{
             "beds": 2,
             "price": 15,
-            "startDate": "2020-05-03",
-            "endDate": "2020-05-10"
+            "startDateReserved": ["03-05-2020","15-5-2020","1-6-2020"],
+            "endDateReserved": ["10-5-2020","22-5-2020","5-6-2020"]
         },
         "room2":{
             "beds": 1,
             "price": 10,
-            "startDate": "2020-06-03",
-            "endDate": "2020-06-10"
+            "startDateReserved": ["03-06-2020"],
+            "endDateReserved": ["10-06-2020"]
         },
         "room3":{
             "beds": 4,
             "price": 30,
-            "startDate": "2020-07-03",
-            "endDate": "2020-07-10"
+            "startDateReserved": ["03-07-2020"],
+            "endDateReserved": ["10-07-2020"]
         },
         "room4":{
             "beds": 3,
             "price": 25,
-            "startDate": "2020-08-03",
-            "endDate": "2020-08-10"
+            "startDateReserved": ["03-08-2020"],
+            "endDateReserved": ["10-08-2020"]
         }
     }
-    startDate = []
-    endDate =[]
 
-res = Reservation()
-
-startDate,endDate = res.makeReservation()
-
-print(startDate)
-print(endDate)
+    def __init__(self):
+        for room in self.roomsAndBeds:            
+            self.startDateReserved.append([[int(y) for y in x.split("-")]for x in self.roomsAndBeds[room]['startDateReserved']])
+            self.endDateReserved.append([[int(y) for y in x.split("-")]for x in self.roomsAndBeds[room]['endDateReserved']])
 
     
+
+
+
+# t = Room()
+# t.displayTheRooms()
+# for x in t.roomsAndBeds['room1']['startDateReserved']:
+#     print(x)
+#     for y in x.split("-"):
+#         print(int(y))
